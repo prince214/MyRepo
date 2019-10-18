b6944fbc9c65ebf99503723a2e749ef99a5dbd4f
Prince Paraste <33788664+prince214@users.noreply.github.com>
2019-10-18 18:39:50 +0530

    added dynamic functionality
    NOTE: Check-List and Delete not working

---
+++ v2/modal_assets/modal.js
@@ -0,0 +1,13 @@
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("my-btn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
